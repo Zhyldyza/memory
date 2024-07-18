b567034230742a6ddee1733710ad26e636e763d0
jyldyzakylbekova <akylbekova@csdisco.com>
2024-07-18 21:14:19 +0600

    game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  {src: "/img/helmet-1.png", matched: false},
+  {src: "/img/potion-1.png", matched: false},
+  {src: "/img/ring-1.png", matched: false},
+  {src: "/img/scroll-1.png", matched: false},
+  {src: "/img/shield-1.png", matched: false},
+  {src: "/img/sword-1.png", matched: false},
+];
 
 function App() {
+  const [cards, setCards]= useState([]);
+  const [turns, setTurns]= useState(0);
+  const [choiceOne, setChoiceOne]=useState(null);
+  const [choiceTwo, setChoiceTwo]=useState(null);
+
+  const shuffleCards=()=>{
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(()=> Math.random() - 0.5)
+    .map((card)=>({...card,id:Math.random()}))
+
+  setCards(shuffledCards);
+  setTurns(0);
+  }
+
+  const handleChoice=(card)=>{
+    choiceOne? setChoiceTwo(card):setChoiceOne(card);
+
+   // console.log(choiceOne, 'first');
+   //console.log(choiceTwo, 'second');
+  };
+
+  const resetTurn=()=>{
+    setChoiceOne(null);
+    setChoiceTwo(null);
+  }
+
+  // compare 2 selected cards
+  useEffect(()=>{
+    if(choiceOne && choiceTwo){
+      if(choiceOne.src === choiceTwo.src){
+        setCards((prevCards)=>{
+          return prevCards.map((card)=>{
+            if(choiceOne.src === card.src){
+              return {...card, matched: true};
+            }
+            return card;
+          });
+        });
+        resetTurn();
+      }
+    }
+    let timerId = setTimeout(()=>{
+      resetTurn();
+    }, 1000);
+
+
+    //cleanup function
+    return ()=>{
+      clearTimeout(timerId);
+    };
+  }, [choiceOne,choiceTwo]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic war</h1>
+      <button onClick={shuffleCards}>New game</button>
+
+      <div className='card-grid'>
+        {cards.map((card)=>(
+           <SingleCard 
+           key={card.id} 
+           card={card} 
+           handleChoice={handleChoice} 
+           flipped={card ===choiceOne || card===choiceTwo || card.matched}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/SingleCard.jsx
@@ -0,0 +1,17 @@
+import "./SingleCard.css";
+
+export default function SingleCard({ card, handleChoice, flipped }) {
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          onClick={() => handleChoice(card)}
+          className="back"
+          src="/img/cover.png"
+          alt="cover"
+        />
+      </div>
+    </div>
+  );
+}
